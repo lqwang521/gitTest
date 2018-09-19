9267e904001458915409c0a50556ffc6024d5da2
王立权 <731696053@qq.com>
2018-09-19 08:34:02 +0800

    项目初始化

---
+++ gitTest/gitTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gitTest
+//
+//  Created by wlq on 2018/9/19.
+//  Copyright © 2018年 htmitech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
